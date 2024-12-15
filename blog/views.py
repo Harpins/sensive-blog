2a2d1ad5e6b62d61beed253aa66b5444499383dc
Igor Derevnin <shroom2207@yandex.ru>
2024-12-15 20:46:42 +0300

    most_popular optimization

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,19 @@ def serialize_post(post):
         'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
+    
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
 
 def serialize_tag(tag):
@@ -72,10 +85,10 @@ def post_detail(request, slug):
     }
 
     all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = all_tags.annotate(tags_count=Count('posts')).order_by('-tags_count')[:5] 
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    all_posts = Post.objects.all().prefetch_related('author')
+    most_popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     context = {
         'post': serialized_post,
@@ -91,10 +104,10 @@ def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
     all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = all_tags.annotate(tags_count=Count('posts')).order_by('-tags_count')[:5] 
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    all_posts = Post.objects.all().prefetch_related('author')
+    most_popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     related_posts = tag.posts.all()[:20]
 
