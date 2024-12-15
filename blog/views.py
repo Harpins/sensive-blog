fd4a221ed53408fdaaf8f1f35e4be2c8367a39ed
Igor Derevnin <shroom2207@yandex.ru>
2024-12-15 21:50:58 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -21,11 +21,11 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
+        'comments_amount': post.comments.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -37,11 +37,17 @@ def serialize_tag(tag):
     }
 
 
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts.count(),
+    }
+
+
 def index(request):
     
-    all_posts = Post.objects.all().prefetch_related('author')
-    most_popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5] 
-
+    all_posts = Post.objects.all().prefetch_related('author', 'comments', 'tags')
+    most_popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
     most_fresh_posts = all_posts.order_by('-published_at')[:5] 
 
     tags = Tag.objects.all()
@@ -49,10 +55,10 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
+        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
